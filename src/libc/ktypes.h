e3ea517cc71f03a2cdceb6d1e359073af6ba6e46
Lion Kortlepel <development@kortlepel.com>
2021-04-01 14:20:22 +0200

    add more broken stuff

---
+++ src/libc/ktypes.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stddef.h>
+
+typedef int8_t i8;
+typedef uint8_t u8;
+typedef int16_t i16;
+typedef uint16_t u16;
+typedef int32_t i32;
+typedef uint32_t u32;
+
