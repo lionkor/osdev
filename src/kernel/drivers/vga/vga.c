5d177cf5ca804d120737ac20a382b123cf7900c1
Lion Kortlepel <development@kortlepel.com>
2021-07-10 10:34:03 +0200

    add driver_utils skeleton, start adding vga ports

---
+++ src/kernel/drivers/vga/vga.c
@@ -0,0 +1,4 @@
+#include "vga.h"
+
+void drv_vga_write_graphics_mode(u8 byte) {
+}
---
+++ src/kernel/drivers/vga/vga.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <ktypes.h>
+
+// these defines exist to make sure we never
+// read or write from/to a port that is totally wrong
+#define DRV_VGA_PORT_3c0 0x3c0
+#define DRV_VGA_PORT_3c2 0x3c2
+#define DRV_VGA_PORT_3c4 0x3c4
+#define DRV_VGA_PORT_3c6 0x3c6
+#define DRV_VGA_PORT_3c7 0x3c7
+#define DRV_VGA_PORT_3c8 0x3c8
+#define DRV_VGA_PORT_3c9 0x3c9
+#define DRV_VGA_PORT_3cc 0x3cc
+#define DRV_VGA_PORT_3ce 0x3ce
+#define DRV_VGA_PORT_3d4 0x3d4
+
+void drv_vga_write_graphics_mode(u8 byte);
